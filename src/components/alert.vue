82669b14f49ed4315213ea2e2ae9f56dcd4bd8ae
songuinho <espoirsongue@85gmail.com>
2022-11-28 16:36:26 +0100

    vujsone 1

---
+++ src/components/alert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div class="message success" v-if="visible">
+        loren ipsum dolor sit consecteur adipisicing elit. A Contactez Unilever
+        et ses équipes de spécialistes ou trouvez des contacts du monde entier.
+      </div>
+    </transition>
+    <button @click="toggle">Toggle</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+    data (){
+        return {
+            visible: true
+        }
+    },
+
+    methods:{
+        toggle: function () {
+            this.visible = !this.visible  
+        }
+    }
+};
+</script>
+
+
+<style>
+ 
+.fade-enter-active .fade-leave-active{
+    transition: opacity .5s, transform 1s;
+}
+
+.fade-enter .fade-leave{
+    opacity: 0;
+    transform: translateX(20px);
+}
+
+</style>
