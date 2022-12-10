bb2610eab37e5e51264e713c2f498cadf2a5a1ce
songuinho <espoirsongue@85gmail.com>
2022-12-10 20:00:48 +0100

    vueone

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Alert></Alert>
   </div>
 </template>
@@ -11,6 +11,8 @@
 import Alert from './components/Alert.vue'
 
 export default {
+  props: {
+  },
   name: 'App',
   components: {
      Alert 
--- src/components/alert.vue
+++ src/components/alert.vue
@@ -1,12 +1,53 @@
+<!-- eslint-disable vue/valid-v-for -->
 <template>
   <div>
-    <transition name="fade">
+    <!-- <transition name="fade" appear>
       <div class="ui message success" v-if="visible">
-        loren ipsum dolor sit consecteur adipisicing elit. A Contactez Unilever
-        et ses équipes de spécialistes ou trouvez des contacts du monde entier.
+        <h1>First text</h1>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </div>
+      
+      <div class="ui message success" v-else>
+        <h1>Second text</h1>
+        <br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
       </div>
     </transition>
-    <button @click="toggle">Toggle</button>
+    <button @click="toggle">Toggle</button> -->
+    <div class="stock">
+      <p v-if="inStock >= 20">{{inStock}} marchandises en stock.</p>
+      <p v-else-if="inStock >= 1 ">{{inStock}} marchandises en stock.</p>
+      <p v-else>Pas de marchandises en stock.</p> 
+    </div>
+    <br />
+    <br />
+    <br />
+    <ul>
+      <li v-for="typeVoiture, index in typeVoitures" :key="typeVoitureId">
+        {{ index+1 }} >> {{ typeVoiture }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="personne, key in personnes" :key="personneId">
+        {{ key+1 }} >> {{ personne.name }} - {{personne.age}} ans - sexe: {{personne.sexe}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,7 +56,25 @@ export default {
   name: "AlertVue",
   data() {
     return {
+
       visible: true,
+
+      typeVoitures: [
+        "Mercedes",
+        "Chevrolet",
+        "Range Rover",
+        "Porche",
+        "BMW GX8",
+      ],
+
+      personnes: [
+        { name: "alex", age: 25, sexe: "M" },
+        { name: "Jovanie", age: 22, sexe: "M" },
+        { name: "Espoir", age: 19, sexe: "M" },
+        { name: "Florida", age: 32, sexe: "F" },
+      ],
+
+      inStock: 0
     };
   },
 
@@ -28,12 +87,19 @@ export default {
 </script>
 
 <style>
-.fade-enter-active .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.5s, transform 1s;
 }
 
-.fade-enter .fade-leave {
+.fade-enter,
+.fade-leave-active {
   opacity: 0;
   transform: translateX(20px);
+}
+
+ul > li{
+  list-style: none;
+  text-align: left;
 }
 </style>
